58d39f24754c8f4c486ae3a167f0a4d9dcb94a7b
panfilo macantan <olifnapnatnacam27.com>
2022-07-24 11:25:54 -0300

    added container && box

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const data = countries;
 
-const firstCountry = countries[6];
-
-const { area, name, capital, population, flag, languages } = firstCountry;
-
-console.log({ area, name, capital, population, flag, languages });
-
-// document.body.innerHTML += `<img src="${flag}" alt="${name}">`;
+const countryContainer = document.querySelector(".country_container");
+let output = "";
 
 for (let i = 0; i < countries.length; i++) {
   const country = countries[i];
   const { name, capital, population, flag } = country;
-  document.body.innerHTML += `<div>
-        <h2>${name}</h2>
-        <p>Capital: ${capital}</p>
-        <p>Population: ${population}</p>
-        <img src="${flag}" height="200" width="200" alt="${name}">
-    </div>`;
+  output += `<div class="box">
+                 <h2>${name}</h2>
+                 <p>Capital: ${capital}</p>
+                 <p>Population: ${population}</p>
+                 <img src="${flag}" height="200" width="200" alt="${name}">
+             </div>`;
 }
+
+countryContainer.innerHTML = output
