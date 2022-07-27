9da68889efd93c85c0955ece2973ff811ba7f04a
panfilo macantan <olifnapnatnacam27.com>
2022-07-27 10:34:10 -0300

    added media query and category

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const data = countries;
 
+const buttonItems = document.querySelectorAll(".category_item");
+let searchValue = undefined;
+let filteredCountries;
+
 const countryContainer = document.querySelector(".country_container");
 let searchInput = document.querySelector("#search_input");
 let output = "";
 
 for (let i = 0; i < countries.length; i++) {
   const country = countries[i];
-  const { name, capital, population, flag } = country;
+  const { name, capital, languages, population, flag, region, area } = country;
   output += `<div class="box">
-                 <h2>${name}</h2>
-                 <p>Capital: ${capital}</p>
-                 <p>Population: ${population}</p>
-                 <img src="${flag}" height="200" width="200" alt="${name}">
+               <img src="${flag}" height="200" width="200" alt="${name}" class="country_image">
+                 <h2 class="country_name">${name}</h2>
+                 <p class="country_capital">Capital: ${capital}</p>
+                 <p class="country_population">Population: ${population}</p>      
+                 <p class="country_languages">Languages: ${languages}</p>  
+                 <p class="country_region">Region: ${region}</p>    
+                 <p class="country_area">Area: ${area}</p>      
              </div>`;
 }
 
@@ -19,23 +26,58 @@ countryContainer.innerHTML = output;
 
 searchInput.addEventListener("keyup", (e) => {
   let output = "";
-  const searchValue = e.target.value.trim().toLowerCase();
+  searchValue = e.target.value.trim().toLowerCase();
   const filterCountries = countries.filter((country) =>
     country.name.toLowerCase().includes(searchValue)
   );
 
   //   const { name, capital, population, flag } = filterCountries;
   filterCountries.forEach((country) => {
-    const { name, capital, population, flag } = country;
+    const { name, capital, languages, population, flag, region, area } =
+      country;
     output += `<div class="box">
-                 <h2>${name}</h2>
-                 <p>Capital: ${capital}</p>
-                 <p>Population: ${population}</p>
-                 <img src="${flag}" height="200" width="200" alt="${name}">
-             </div>`;
+                <img src="${flag}" height="200" width="200" alt="${name}" class="country_image">
+                  <h2 class="country_name">${name}</h2>
+                  <p class="country_capital">Capital: ${capital}</p>
+                  <p class="country_population">Population: ${population}</p>      
+                  <p class="country_languages">Languages: ${languages}</p>      
+                  <p class="country_region">Region: ${region}</p>      
+                  <p class="country_area">Area: ${area}</p>      
+              </div>`;
   });
 
   countryContainer.innerHTML = output;
 
   console.log(filterCountries);
+});
+
+document.querySelector(
+  ".country_length"
+).innerText = `We have ${data.length} countries on the world.`;
+
+buttonItems.forEach(function (item) {
+  item.addEventListener("click", function () {
+    switch (this.innerText) {
+      case "Country":
+        filteredCountries = countries.filter((country) =>
+          country.name.toLowerCase().includes(searchValue)
+        );
+
+        break;
+      case "Capital":
+        filteredCountries = countries.filter((country) =>
+          country.capital.toLowerCase().includes(searchValue)
+        );
+
+        break;
+      case "Region":
+        filteredCountries = countries.filter((country) =>
+          country.region.toLowerCase().includes(searchValue)
+        );
+
+        break;
+      default:
+        console.log("No Country found!");
+    }
+  });
 });
